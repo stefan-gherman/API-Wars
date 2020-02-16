f91c16c49e2d09de0e4edadc926cc88d2d7fb38f
Stefan Gherman <scgheman@gmail.com>
2020-02-16 17:21:18 +0200

    Add login route

--- server.py
+++ server.py
@@ -1,14 +1,24 @@
 from flask import Flask, request, redirect, url_for, session, render_template
-from psycopg2 import sql
-from werkzeug.security import generate_password_hash, check_password_hash
-
+import queries
 app = Flask(__name__)
 
 
-@app.route('/')
-def login():
+@app.route('/', methods=['GET','POST'])
+def home():
+
     return render_template('login_page.html')
 
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    username = request.values.get('username')
+    password = request.values.get('pass')
+
+    if queries.add_user(username, password) is not False:
+        return render_template('base.html')
+    else:
+        print('User already exists')
+        return  None
 
 
 if __name__ == '__main__':
