882f5d09dc718b52d18145f2d7b6524753401a18
Stefan Gherman <scgheman@gmail.com>
2020-02-27 03:22:48 +0200

    Add code for heroku connection.

--- db_connection.py
+++ db_connection.py
@@ -1,34 +1,44 @@
 import os
 import psycopg2
 import psycopg2.extras
+import urllib
 
 
 def get_connection_string():
+    pass
     # setup connection string
     # to do this, please define these environment variables first
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
+    # user_name = os.environ.get('PSQL_USER_NAME')
+    # password = os.environ.get('PSQL_PASSWORD')
+    # host = os.environ.get('PSQL_HOST')
+    # database_name = os.environ.get('PSQL_DB_NAME')
+    #
+    # env_variables_defined = user_name and password and host and database_name
+    #
+    # if env_variables_defined:
+    #     # this string describes all info for psycopg2 to connect to the database
+    #     return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
+    #         user_name=user_name,
+    #         password=password,
+    #         host=host,
+    #         database_name=database_name
+    #     )
+    # else:
+    #     raise KeyError('Some necessary environment variable(s) are not defined')
 
-    env_variables_defined = user_name and password and host and database_name
-
-    if env_variables_defined:
-        # this string describes all info for psycopg2 to connect to the database
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-            user_name=user_name,
-            password=password,
-            host=host,
-            database_name=database_name
-        )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
-
-
+urllib.parse.uses_netloc.append('postgres')
+url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
 def open_database():
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        # connection_string = get_connection_string()
+        # connection = psycopg2.connect(connection_string)
+        connection = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
